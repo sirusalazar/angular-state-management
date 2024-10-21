1d270a7affbd8658555b5e22fa0e3611b879e8ae
sirusalazar <sirusalazar@gmail.com>
2024-10-20 21:09:45 -0500

    FIX: added redirection after submit

--- src/app/store/effects/cart.effects.ts
+++ src/app/store/effects/cart.effects.ts
@@ -2,9 +2,10 @@ import { inject } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { CartActions, ProductsPageActions } from '@app-store/actions';
 import { debounceTime, map } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const submitOrder$ = createEffect(
-  (actions$ = inject(Actions)) => {
+  (actions$ = inject(Actions), router = inject(Router)) => {
     return actions$.pipe(
       ofType(CartActions.submitOrder),
       debounceTime(1000),
@@ -14,6 +15,7 @@ export const submitOrder$ = createEffect(
           'color:red; font-size:20px;',
           order
         );
+        router.navigate(['']);
         return ProductsPageActions.updateProductsStock({ order });
       })
     );
